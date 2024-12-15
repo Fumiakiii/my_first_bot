ceb7e3668765a3acca64999698e80d3d1ece723d
Fumiakiii <takahashi112358@gmail.com>
2024-12-15 15:23:17 +0900

    final2

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text + "はい！！"))
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=event.message.text + "村岡泰平！村岡泰平！村岡泰平！")
+    )
 
 
 if __name__ == "__main__":
